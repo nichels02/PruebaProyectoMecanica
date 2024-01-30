a4dff9cb8c21e43543475f8db6c53f52eab66f7b
Tiwazw-w <nicolas.derivero@tecsup.edu.pe>
2024-01-30 16:20:28 -0500

    Revert "xd"
    This reverts commit 7726fa068cbbcc54317ed08540b66823955850b9.

--- Assets/Scripts/Pruebas/NoTocar.cs
+++ Assets/Scripts/Pruebas/NoTocar.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//public enum Piezas { pieza1, pieza2, pieza3, pieza4, pieza5, pieza6, pieza7 }
+
 public class NoTocar : MonoBehaviour
 {
-    //public Piezas typePieza;
     [SerializeField] string tagReferences;
     public bool colisionDetectada = false;
     Rigidbody rb;
