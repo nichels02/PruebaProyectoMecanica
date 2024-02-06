332e82c2baa1791c2446945f8a58d03c9dd1aeb0
Tiwazw-w <nicolas.derivero@tecsup.edu.pe>
2024-02-06 13:43:51 -0500

    ga

--- Assets/Scripts/GameManager 2.cs
+++ Assets/Scripts/GameManager 2.cs
@@ -14,37 +14,3 @@ public class GameManager2 : MonoBehaviour
     }
 }
 
-
-/*
-using UnityEngine;
-
-public class PadreScript : MonoBehaviour
-{
-    private void Start()
-    {
-        // Acceder al primer hijo del objeto padre
-        Transform primerHijo = transform.GetChild(0);
-
-        if (primerHijo != null)
-        {
-            // Obtener el componente del script en el primer hijo
-            HijoScript hijoScript = primerHijo.GetComponent<HijoScript>();
-
-            if (hijoScript != null)
-            {
-                // Realizar cambios en el script del hijo
-                hijoScript.ModificarCodigo();
-            }
-            else
-            {
-                Debug.LogError("El primer hijo no tiene el script HijoScript adjunto.");
-            }
-        }
-        else
-        {
-            Debug.LogError("El objeto padre no tiene hijos.");
-        }
-    }
-}
-
-*/
