3d702a527149f2a8c46a9f939cc81375ed5e743d
Tiwazw-w <nicolas.derivero@tecsup.edu.pe>
2024-02-09 10:23:57 -0500

    RotacionCalculada

--- Assets/PruebaN/MovimientoPreciso.cs
+++ Assets/PruebaN/MovimientoPreciso.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 
 public class MovimientoPreciso : MonoBehaviour
 {
-    Rotacion LaRotacion;
+    [SerializeField] Rotacion LaRotacion;
     int velocidad = 100;
     [SerializeField] Vector3 ElVectorRotacion;
-    Vector3 ElVectorRotacionComprobacion;
+    [SerializeField] int vueltas;
+    Vector3 ElVectorRotacionComprobacion = new Vector3(1, 1, 1);
     [SerializeField] bool debeRotar;
+    bool debeRotar2;
+
 
     private void Start()
     {
-        LaRotacion = GetComponent<Rotacion>();
+        ElVectorRotacionComprobacion.x = ElVectorRotacion.x != 0 ? ElVectorRotacionComprobacion.x = 360 / ElVectorRotacion.x * vueltas * ElVectorRotacion.x : 0;
+        ElVectorRotacionComprobacion.y = ElVectorRotacion.y != 0 ? ElVectorRotacionComprobacion.y = 360 / ElVectorRotacion.y * vueltas * ElVectorRotacion.y : 0;
+        ElVectorRotacionComprobacion.z = ElVectorRotacion.z != 0 ? ElVectorRotacionComprobacion.z = 360 / ElVectorRotacion.z * vueltas * ElVectorRotacion.z : 0;
+
+        debeRotar = true;
+        
     }
 
     private void Update()
     {
-        transform.rotation = LaRotacion.calcularRotacion(ElVectorRotacion);
-        if (debeRotar)
+        //print("1");
+        //ElVectorRotacion= 5,0,0
+
+
+
+        if (debeRotar == true)
         {
+            debeRotar2 = true;
+            //ElVectorRotacionComprobacion = ElVectorRotacion * 10000;
+            debeRotar = false;
+        }
+        else if (debeRotar2 == true && ElVectorRotacionComprobacion.x > 0 || ElVectorRotacionComprobacion.y > 0 || ElVectorRotacionComprobacion.z > 0)
+        {
+            print(ElVectorRotacionComprobacion);
             rotar();
         }
+        
     }
 
 
     void rotar()
     {
-        print(ElVectorRotacion);
-        Vector3 elVector = ElVectorRotacion * Time.deltaTime * velocidad;
-        ElVectorRotacionComprobacion += elVector;
-        transform.rotation = LaRotacion.calcularRotacion(elVector);
-        if (ElVectorRotacion.x < 0 && ElVectorRotacion.y<0 && ElVectorRotacion.z < 0)
-        {
-            debeRotar = false;
-        }
+        ElVectorRotacionComprobacion -= ElVectorRotacion;
+        Quaternion currentRotation = transform.rotation;
+        Quaternion deltaRotation = LaRotacion.calcularRotacion(ElVectorRotacion);
+
+        transform.rotation = currentRotation * deltaRotation;
     }
 
 
