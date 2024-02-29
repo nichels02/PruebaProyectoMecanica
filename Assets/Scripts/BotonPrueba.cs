43d3e4fabdaff593b0e9c31aa72ac9fdfafddcea
Tiwazw-w <nicolas.derivero@tecsup.edu.pe>
2024-02-29 09:05:59 -0500

    gaa

---
+++ Assets/Scripts/BotonPrueba.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonPrueba : MonoBehaviour
+{
+    public void xd()
+    {
+        print("gaaa");
+    }
+}
